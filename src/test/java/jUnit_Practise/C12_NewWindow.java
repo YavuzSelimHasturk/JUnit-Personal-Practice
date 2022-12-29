ce8ccb294feb7e202125c3f3c521b4e2bec4edcd
YavuzSelimHasturk <yavuzselimhasturk@gmail.com>
2022-12-29 11:12:42 +0300

    New Window Test

---
+++ src/test/java/jUnit_Practise/C12_NewWindow.java
@@ -0,0 +1,48 @@
+package jUnit_Practise;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class C12_NewWindow {
+    static WebDriver driver;
+
+    @Before
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+    }
+    @After
+    public void teardown() throws InterruptedException {
+        Thread.sleep(5000);
+        driver.quit();
+    }
+    @Test
+    public void test() throws InterruptedException {
+        driver.get("https://www.ebay.com");
+        String ebayWindowHandleDegeri = driver.getWindowHandle();
+
+        driver.switchTo().newWindow(WindowType.TAB).get("https://www.youtube.com");
+
+        String expectedStr = "YouTube";
+        String actualTitle = driver.getTitle();
+        Assert.assertEquals(expectedStr,actualTitle);
+        Thread.sleep(5000);
+
+        driver.switchTo().window(ebayWindowHandleDegeri);
+        expectedStr="ebay";
+        String actualURL= driver.getCurrentUrl();
+        Assert.assertTrue(actualURL.contains(expectedStr));
+        Thread.sleep(5000);
+
+    }
+}
